334d468bde2b585279c9466cace92df94ed614aa
lakshya404stc <lakshyag404stc.com>
2023-11-20 17:03:55 +0530

    Initial commit

---
+++ server/database/db.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+const Connection = () => {
+
+    const MONGODB_URI = `mongodb://127.0.0.1:27017/todo`;
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
